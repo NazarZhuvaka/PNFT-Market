ef19b600b0429a460f9bc9d84f178b7a00d53dc2
NazarZhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2023-12-08 18:59:18 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthHeaderComponent } from './auth-header/auth-header.component';
+import { AuthFooterComponent } from './auth-footer/auth-footer.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Observable } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthHeaderComponent,
+    AuthFooterComponent,
+    AuthFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
+
+
